21555441a1e786d2762ff191ec94ad483126f15d
kitkat <kitka@DESKTOP-FB43FD0>
2024-07-04 19:07:14 +0100

    Styling updates

--- src/Tile/Tile.tsx
+++ src/Tile/Tile.tsx
@@ -1,8 +1,10 @@
+import "./tile.scss"; 
+
 export default function Tile({name}) {
     return (
     <div className="col border">
-        <div>
-            <p className="d-sm-block">
+        <div className="bingo-card__square">
+            <p className="lead font-weight-normal">
                 {name}
             </p>
         </div>
