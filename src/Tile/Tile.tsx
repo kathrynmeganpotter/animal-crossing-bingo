c0e002f7fd1c0eb1acdf9aebaa466c7c8a49a7c7
Kathryn Potter <kathrynmeganpotter@gmail.com>
2025-11-01 11:58:03 +0000

    Update selected style

--- src/Tile/Tile.tsx
+++ src/Tile/Tile.tsx
@@ -1,13 +1,19 @@
-import "./tile.scss"; 
+import "./tile.scss";
 
-export default function Tile({name, onTileClick, selected}) {
-    return (
+export default function Tile({ name, onTileClick, selected }) {
+  return (
     <div className="col border">
-        <div className={`bingo-card__square ${selected ? "bingo-card__selected" : ""}`} onClick={onTileClick}>
-            <p className="lead font-weight-normal">
-                {name}
-            </p>
-        </div>
+      <div
+        className='bingo-card__square'
+        onClick={onTileClick}
+      >
+        {selected && (
+          <span className="bingo-card__square--selected">
+            ✘
+          </span>
+        )}
+        <p className="lead font-weight-normal">{name}</p>
+      </div>
     </div>
-    )
-  }
+  );
+}
